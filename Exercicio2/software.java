58f45fe0c09e0d5273fd3898c16a3bd2b07d295e
joaovittor07 <119264762+joaovittor07@users.noreply.github.com>
2024-03-21 21:16:49 -0300

    Add files via upload

---
+++ Exercicio2/software.java
@@ -0,0 +1,75 @@
+package Exercicio2;
+
+import java.util.Date;
+
+public class software <S> {
+    private S id;
+    private double valor;
+    private Date dataFabricacao;
+    private Date dataVencimento;
+
+    public software(S id, double valor, Date dataFabricacao, Date dataVencimento) {
+        this.id = id;
+        this.valor = valor;
+        this.dataFabricacao = dataFabricacao;
+        this.dataVencimento = dataVencimento;
+    }
+
+       
+	public S getId() {
+		return id;
+	}
+
+
+	public void setId(S id) {
+		this.id = id;
+	}
+
+
+	public double getValor() {
+		return valor;
+	}
+
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+
+	public Date getDataFabricacao() {
+		return dataFabricacao;
+	}
+
+
+	public void setDataFabricacao(Date dataFabricacao) {
+		this.dataFabricacao = dataFabricacao;
+	}
+
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Produto [id=");
+		builder.append(id);
+		builder.append(", valor=");
+		builder.append(valor);
+		builder.append(", dataFabricacao=");
+		builder.append(dataFabricacao);
+		builder.append(", dataVencimento=");
+		builder.append(dataVencimento);
+		builder.append("]");
+		return builder.toString();
+	}
+   
+}
